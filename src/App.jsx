f35befd19da17a30013de576f742be730afb9a5e
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-06-23 06:29:48 +0330

    feat: create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import RootLayout from "./layouts/RootLayout"
+import Home from "./pages/Home"
+import Create from "./pages/Create"
+import Edit from "./pages/Edit"
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <RootLayout />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: "create",
+				element: <Create />,
+			},
+			{
+				path: "/edit/:id",
+				element: <Edit />,
+			},
+		],
 	},
 ])
 
